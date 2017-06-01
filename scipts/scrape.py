ac8c2b88cd892c05c6900456e2b8475d2457eef9
Udit agarwal <uditagarwal1997@gmail.com>
2017-06-01 15:32:45 +0530

    Updated scrape.py

--- scipts/scrape.py
+++ scipts/scrape.py
@@ -7,4 +7,16 @@ me = travis_user(key)
 url=raw_input("Enter github repository url");
 if me.init_repo(url):
     builds = me.get_build()
-    print builds
+    for build in builds:
+        if build.state == 'failed':
+            custom_build = me.user.build(build.id)
+            failed_jobs = custom_build.job_ids
+            for job_id in failed_jobs:
+                job = me.user.job(int(job_id))
+                log = job.log.body
+                log = log.encode('utf-8')
+                fh = open(str(job.log_id)+".txt","w+")
+                fh.write(log)
+                fh.close
+                print("Log written")
+            
