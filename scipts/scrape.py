e21b34caef3218a944b59162c9b6a65c543d2e5e
Udit agarwal <uditagarwal1997@gmail.com>
2017-06-03 09:09:18 +0530

    Updated scrape.py! Errors fetched

--- scipts/scrape.py
+++ scipts/scrape.py
@@ -1,5 +1,10 @@
 from travis_user import travis_user
+import os
 
+def get_error_snippet(error, text):
+    error_index =  text.index(error)
+    return text[error_index-150:error_index+100]
+    
 handle = open("key.txt","r")
 key = handle.readline()
 handle.close()
@@ -7,6 +12,8 @@ me = travis_user(key)
 url=raw_input("Enter github repository url");
 if me.init_repo(url):
     builds = me.get_build()
+    print("build retrieved")
+    os.chdir(os.getcwd()+'/logs/')
     for build in builds:
         if build.state == 'failed':
             custom_build = me.user.build(build.id)
@@ -19,4 +26,16 @@ if me.init_repo(url):
                 fh.write(log)
                 fh.close
                 print("Log written")
-            
+            for content in os.listdir(os.getcwd()):
+                try:
+                    text = open(content,'r').read()
+                except:
+                    print('cannot open file, skipping!!')
+                error_type = open('error_list.txt', 'r').read()
+                for error in error_type.split(','):
+                    if (str(error) in text) and error!=' ' and error!='\n':
+                        print get_error_snippet(error, text)
+                        print('-------------------------------------------')
+                fh.close()
+        else:
+            print("Build passed!! no error")
